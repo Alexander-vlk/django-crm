4247306ddd83b3088fb32c00ae9b7c42a355658b
alex_barmen <www.lad282@gmail.com>
2024-10-07 17:44:16 +0300

    frat: added auth_service app to project

---
+++ crm/auth_service/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+app_name = 'auth_service'
+urlpatterns = [
+    
+]
--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'auth_service.apps.AuthServiceConfig',
 ]
 
 MIDDLEWARE = [
