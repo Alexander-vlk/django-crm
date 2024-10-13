708ac8faeafc6819fa1731d0ec55a1b30ba67a17
alex_barmen <www.lad282@gmail.com>
2024-10-13 19:43:42 +0300

    feat: added page name to dashboard_index view

--- crm/dashboard/views.py
+++ crm/dashboard/views.py
@@ -5,5 +5,7 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard_index(request):
     template_name = 'dashboard/dashboard.html'
-    context = {}
+    context = {
+        'page_name': 'Панель управления',
+    }
     return render(request, template_name, context)
